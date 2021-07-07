d00b0de6bb31f1fc2c443fe289f929215ca07231
Everton Pinheiro <evertonpinheiroti@gmail.com>
2021-07-07 10:54:06 -0300

    :sparkles: Created room code button

---
+++ src/components/RoomCode/index.tsx
@@ -0,0 +1,22 @@
+import * as Styled from './styles';
+
+import copyImg from '../../assets/images/copy.svg';
+
+type RoomCodeProps = {
+  code: string
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
+  return (
+    <Styled.Container onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </Styled.Container>
+  )
+}
