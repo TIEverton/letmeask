64ab844dee915fdc7b683bd35e785f8330da080c
Everton Pinheiro <evertonpinheiroti@gmail.com>
2021-07-09 09:27:10 -0300

    :sparkles: Added likes functionality

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type QuestionProps = {
   id: string;
@@ -10,6 +11,8 @@ type QuestionProps = {
   content: string
   isAnswered: boolean
   isHighlighted: boolean
+  likeCount: number
+  likeId: string | undefined
 }
 
 type FirebaseQuestions = Record<string, {
@@ -20,9 +23,13 @@ type FirebaseQuestions = Record<string, {
   content: string
   isAnswered: boolean
   isHighlighted: boolean
+  likes: Record<string, {
+    authorId: string
+  }>
 }>
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionProps[]>([])
   const [title, setTitle] = useState('');
 
@@ -39,7 +46,9 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
         }
       })
 
@@ -47,7 +56,11 @@ export function useRoom(roomId: string) {
       setQuestions(parsedQuestions);
     })
 
-  }, [roomId])
+
+    return (() => {
+      roomRef.off('value')
+    })
+  }, [roomId, user?.id])
 
   return { questions, title }
 }
